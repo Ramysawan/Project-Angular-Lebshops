438c408ca09348e885385224920bfdc885369234
Ramysawan <ramy_sawan@Hotmail.com>
2020-12-02 14:29:40 +0200

    Lebshops

---
+++ Lebshops/src/app/components/liquors/liquors.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LiquorsComponent } from './liquors.component';
+
+describe('LiquorsComponent', () => {
+  let component: LiquorsComponent;
+  let fixture: ComponentFixture<LiquorsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LiquorsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LiquorsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
